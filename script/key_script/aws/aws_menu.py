43d843193bcd52b99e5ebe173e59e1a0df427b92
Rohit Raut <rohitraut3366@gmail.com>
2021-05-17 11:17:59 +0530

    formatting

--- script/key_script/aws/aws_menu.py
+++ script/key_script/aws/aws_menu.py
@@ -1,27 +1,24 @@
 import os
 
-from compute.compute import compute
+from key_script.aws.compute.compute import compute
 
 
 def Aws():
-
     os.system("aws configure")
     while True:
         os.system('tput setaf 4')
         print('''
-                Press 1: Compute
-                Press 2: Networking and Content Deliver
-                Press 3: Exit from this menu
+\tPress 1: Compute\n\tPress 2: Networking and Content Deliver # In Progress\n\tPress 3: Exit from this menu
                 ''')
         os.system('tput setaf 7')
-        choice = input("\n Enter Your Choice:")
+        choice = input("\tEnter Your Choice:")
         if choice == '1':
-            compute()    
+            compute()
         elif choice == '2':
             pass
         else:
-            if choice != '3':
-                print("Wrong choice")
-            return
+            if choice == '3':
+                print("\tWrong choice")
+                return
+            print("\tTry Again")
         os.system("clear")
-Aws()
