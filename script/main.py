ec9840579c2c0a0254a9112f6f094f3beb553b09
Rohit Raut <rohitraut3366@gmail.com>
2021-05-27 09:04:15 +0530

    kube config

--- script/main.py
+++ script/main.py
@@ -2,6 +2,7 @@ import getpass
 
 from key_script.aws.aws_menu import Aws
 from key_script.docker.docker import docker_main
+from key_script.git_gitHub.git_gitHub import github
 from key_script.hadoop.hadoop import HadoopMainMenu
 from key_script.kubernetes.kubernetes import kube_menu
 from key_script.linux.linux_menu import linux
@@ -14,7 +15,7 @@ if path.lower() == 'voice':
     pass
 elif path.lower() == 'keyboard':
     while True:
-        print("Press 1 : AWS\nPress 2 : Docker\nPress 3 : Hadoop\nPress 4 : Kubernetes\nPress 5 : Linux/web server")
+        print("Press 1 : AWS\nPress 2 : Docker\nPress 3 : Hadoop\nPress 4 : Kubernetes\nPress 5 : Linux/web server\n Press 6: Git Actions")
 
         choice = input("\nEnter your choice: ")
         if choice == '1':
@@ -27,6 +28,8 @@ elif path.lower() == 'keyboard':
             kube_menu()
         elif choice == '5':
             linux()
+        elif choice == '6':
+            github()
         elif choice == '6':
             exit(0)
         else:
